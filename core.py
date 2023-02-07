c87500a1e0f04973fb0fad143defcb24c3fc1944
Thomas <thwr@itu.dk>
2023-02-07 17:09:57 +0100

    Change variable name to increase descriptiveness

--- core.py
+++ core.py
@@ -14,23 +14,23 @@ def test():
         savedir=tmpdir,
     )
 
-    defaultVoice, fs = torchaudio.load("VoiceSamples/Hello_speechbrain.wav")
+    templateVoice, fs = torchaudio.load("VoiceSamples/Hello_speechbrain.wav")
     sameVoice, fs = torchaudio.load("VoiceSamples/What_is_lorem_ipsum.wav")
     # testVoice, fs = torchaudio.load("VoiceSamples/der_kommer_spyd.flac")
     testVoice, fs = torchaudio.load("VoiceSamples/test.wav")
 
 
     
-    print("Default Voice")
-    score, prediction = verification.verify_batch(defaultVoice, defaultVoice)
+    print("Same template voice file")
+    score, prediction = verification.verify_batch(templateVoice, templateVoice)
     print(prediction, score)
 
-    print("Default and Same Voice")
-    score, prediction = verification.verify_batch(defaultVoice, sameVoice)
+    print("Template and same Voice")
+    score, prediction = verification.verify_batch(templateVoice, sameVoice)
     print(prediction, score)
 
-    print("different voice")
-    score, prediction = verification.verify_batch(defaultVoice, testVoice)
+    print("different voice" files)
+    score, prediction = verification.verify_batch(templateVoice, testVoice)
     print(prediction, score)
      
 test()
