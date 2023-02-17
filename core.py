4d08612fdd1ab006357b81bbe6d31debfe523ff4
ThomasITU <thwr@itu.dk>
2023-02-17 02:58:51 +0100

    add sanity check function and a timing wrapper
    To evaluate time of methods without creation overhead

--- core.py
+++ core.py
@@ -1,3 +1,7 @@
+import time
+from functools import wraps
+import gc
+
 import speechbrain as sb
 from IPython.display import Audio
 from speechbrain.pretrained import SpeakerRecognition
@@ -28,17 +32,31 @@ class VoiceAuthentication:
 voiceHandler = VoiceAuthentication()
 
 
-    
-    print("Same template voice file")
-    score, prediction = verification.verify_batch(templateVoice, templateVoice)
-    print(prediction, score)
+def timeit(func):
+    gc.disable()
+    @wraps(func)
+    def timeit_wrapper(*args, **kwargs):
+        start_counter_ns = time.perf_counter_ns()
+        result = func(*args, **kwargs)
+        end_counter_ns = time.perf_counter_ns()
+        total_time = (end_counter_ns - start_counter_ns)/10**9
+        print(f'Function {func.__name__}{args} {kwargs} Took {total_time:.4f} seconds')
+        return result
+    gc.enable()
+    return timeit_wrapper
 
-    print("Template and same Voice")
-    score, prediction = verification.verify_batch(templateVoice, sameVoice)
-    print(prediction, score)
 
-    print("different voice files")
-    score, prediction = verification.verify_batch(templateVoice, testVoice)
-    print(prediction, score)
-     
-test()
+@timeit
+def test(voiceSample:str, voiceHandler:VoiceAuthentication, profileSamples:list[str]):
+    scoreArray = voiceHandler.GenerateScoreArray(voiceSample, profileSamples)
+    print(f"score array:{scoreArray}")
+    print(f"average: {sum(scoreArray)/len(scoreArray)}\n")
+
+
+input("Press enter to start")
+if __name__ == '__main__':
+    profileSamples = ["VoiceSamples/Hello_speechbrain.wav", "VoiceSamples/Hello_speechbrain.wav","VoiceSamples/What_is_lorem_ipsum.wav" ]
+    test("VoiceSamples/What_is_lorem_ipsum.wav", voiceHandler, profileSamples)
+
+    profileSamples2 = ["VoiceSamples/test.wav"]          
+    test("VoiceSamples/What_is_lorem_ipsum.wav", voiceHandler, profileSamples2)
