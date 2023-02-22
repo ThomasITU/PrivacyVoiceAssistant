7a23dc8d8b0373677b394818e81ec89b9cfcaef3
ThomasITU <thwr@itu.dk>
2023-02-22 17:08:18 +0100

    add handle script and compose file

---
+++ handle.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Available environment variables
+# -------------------------------
+# Base directory of Rhasspy: ${RHASSPY_BASE_DIR}
+# Name of current profile: ${RHASSPY_PROFILE}
+# Profile directory: ${RHASSPY_PROFILE_DIR}
+
+# Output should be JSON
+cat >> /tmp/intentCat.json
