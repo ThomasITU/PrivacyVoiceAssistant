56e82e9a3504831b0535308a0b5c43810a98fd0a
ThomasITU <thwr@itu.dk>
2023-03-21 01:07:08 +0100

    remove unnessary var and move class
    for readability

--- src/services/BluetoothClient.py
+++ src/services/BluetoothClient.py
@@ -16,13 +16,6 @@ def printNearbyDevices():
     for device in devices:
         print(device)
 
-def connectToDevice(port:int, name:str, targetMacAddress) -> bluetooth.BluetoothSocket:
-    print("Connecting to \"{}\" on {}".format(name, targetMacAddress))
-    sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
-    sock.connect((targetMacAddress, port))
-    print("Connected")
-    return sock
-
 def connectToNearbyDevice(name="VoiceAssistant", uuid="94f39d29-7d6d-437d-973b-fba39e49d4ee", targetMacAddress=None) -> bluetooth.BluetoothSocket:
     try:
         if len(targetMacAddress) == 0:
@@ -32,13 +25,20 @@ def connectToNearbyDevice(name="VoiceAssistant", uuid="94f39d29-7d6d-437d-973b-f
         if len(service_matches) == 0:
             print(f"Couldn't find the service {name}, {uuid} with address: {targetMacAddress}")
             return None
+        
         first_match = service_matches[0]
         port = first_match["port"]
-        name = first_match["name"]
         targetMacAddress = first_match["host"]
         return connectToDevice(port, name, targetMacAddress)
     except any as e:
         print(e)
+
+def connectToDevice(port:int, name:str, targetMacAddress) -> bluetooth.BluetoothSocket:
+    print("Connecting to \"{}\" on {}".format(name, targetMacAddress))
+    sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+    sock.connect((targetMacAddress, port))
+    print("Connected")
+    return sock
 
 def sendProfile(sock:BluetoothSocket, profile = _.dummyProfile()):
     while "y" in input("Send profile? (y/n): ").lower():
@@ -51,7 +51,7 @@ def main():
     while "y" in input("Print nearby devices? (y/n): ").lower():
         printNearbyDevices()
     
-    targetMacAddress = input("Enter the address of the device you want to connect to: ")
+    targetMacAddress = input("Enter the address of the device you want to connect to (leave blank if unsure): ")
     sock = connectToNearbyDevice(targetMacAddress=targetMacAddress)
     sendProfile(sock)
     print("Done")
