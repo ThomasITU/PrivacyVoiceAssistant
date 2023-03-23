2ec25629aef5c6edc7b20ea27876728932102aab
ThomasITU <thwr@itu.dk>
2023-03-23 12:58:36 +0100

    remove unnecessary file and volumes

--- .devcontainer/scripts/SetupBluetooth.sh
+++ .devcontainer/scripts/SetupBluetooth.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 apt-get install libbluetooth-dev -y
-apt-get install git -y
 apt-get install bluez -y
+apt-get install dbus -y
 
 pip3.10 install --upgrade pip setuptools==57.5.0
 pip3.10 install git+https://github.com/pybluez/pybluez.git#egg=pybluez
@@ -10,5 +10,7 @@ mkdir /var/run/sdp
 
 service dbus start
 service bluetooth start
-hciconfig hci0 piscan
+hciconfig hci0 down
+hciconfig hci0 up
 sdptool add SP
+hciconfig hci0 piscan
--- .devcontainer/scripts/SetupDependencies.sh
+++ .devcontainer/scripts/SetupDependencies.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/env bash
 
 # create a directory for apt to store its cache
+
 path="/var/cache/apt"
+if [ -d "$directory" ]; then
+    echo "$directory exists."
+else 
+    wget https://www.python.org/ftp/python/3.10.8/Python-3.10.8.tgz -O /tmp/Python-3.10.8.tgz
+    cd /tmp/
+    tar xzf Python-3.10.8.tgz 
+    rm Python-3.10.8.tgz
+    cd Python-3.10.8 
+    ./configure --enable-optimizations 
+    make altinstall 
+    cd ../../privacyVoiceAssistant/src/
+fi
 mkdir $path
 mkdir $path/amd64
 mkdir $path/amd64/archives
@@ -11,5 +24,5 @@ apt install vim -y
 echo "installing python"
 source /privacyVoiceAssistant/scripts/InstallPython.sh
 
-echo "setting up bluetooth"
-source /privacyVoiceAssistant/scripts/SetupBluetooth.sh
+# echo "setting up bluetooth"
+# source /privacyVoiceAssistant/scripts/SetupBluetooth.sh
