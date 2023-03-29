7f9ce0a2db07fa7256e41ad344dd105ee442e03d
ThomasITU <thwr@itu.dk>
2023-03-29 23:23:54 +0200

    update install script and requirements.txt

--- .devcontainer/scripts/InstallPython.sh
+++ .devcontainer/scripts/InstallPython.sh
@@ -6,12 +6,12 @@ apt install wget build-essential libncursesw5-dev libssl-dev -y\
 
 # download and install python 3.10.8
 
-directory=/tmp/Python-3.10.8 
+directory=/privacyVoiceAssistant/lib/Python-3.10.8 
 if [ -d "$directory" ]; then
     echo "$directory exists."
 else 
-    wget https://www.python.org/ftp/python/3.10.8/Python-3.10.8.tgz -O /tmp/Python-3.10.8.tgz
-    cd /tmp/
+    wget https://www.python.org/ftp/python/3.10.8/Python-3.10.8.tgz -O /privacyVoiceAssistant/lib//Python-3.10.8.tgz
+    cd /privacyVoiceAssistant/lib/
     tar xzf Python-3.10.8.tgz 
     rm Python-3.10.8.tgz
     cd Python-3.10.8 
@@ -21,6 +21,4 @@ else
 fi
 
 
-pip3.10 install ipython
-pip3.10 install jsonpickle
-# pip3 install -r --file requirements.txt
+pip3.10 install -r --file requirements.txt
