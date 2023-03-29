abd8015dd720317db2bc78633b2dbc341e8c69ab
ThomasITU <thwr@itu.dk>
2023-03-29 11:17:00 +0200

    refactor to increase clarity and control flow

--- src/handlers/handle.py
+++ src/handlers/handle.py
@@ -5,32 +5,36 @@ import sys
 import json
 import random
 import datetime
+import uuid
+from handlers.PolicyHandler import PolicyHandler
 
+from services.VoiceAuthentication import VoiceAuthentication
 
-def saveIntentToFile(intent:str):
+ENDPOINT = "http://localhost:12101/api/play-recording"
+PATH = "/tmp/voiceFiles/"
+global o
+def save_intent_to_file(intent:str, fileName:str):
     f = open("/tmp/lastIntent.txt", "w")
     f.write(intent + "\n")
     f.close()
 
-
-ENDPOINT = "http://localhost:12101/api/play-recording"
-PATH = "/tmp/voiceFiles/"
-FILE_NAME = PATH+"lastCommand.wav"
-
-def speech(text):
-    global o
-    o["speech"] = {"text": text}
-    saveIntentToFile(text)
-
-
-# get json from stdin and load into python dict
-o = json.loads(sys.stdin.read())
-
-intent = o["intent"]["name"]
-
-process = subprocess.Popen(['curl', '--output', FILE_NAME, ENDPOINT], 
+def save_voice_file() -> str:
+    fileName = PATH+uuid.uuid4()+".wav"
+    process = subprocess.Popen(['curl', '--output', fileName, ENDPOINT], 
                            stdout=subprocess.PIPE,
                            universal_newlines=True)
+    return fileName
+
+def get_intent(file_name:str) -> str:
+    # get json from stdin and load into python dict
+    o = json.loads(sys.stdin.read())
+
+    intent = o["intent"]["name"]
+    return intent
+
+def speech(text):
+    o["speech"] = {"text": text}
+
 
 
 if intent == "GetTime":
@@ -41,12 +45,16 @@ elif intent == "Hello":
     replies = ['Hi!', 'Hello!', 'Hey there!', 'Greetings.']
     speech(random.choice(replies))
 
-elif intent == "LongSentence":
-    response = "This is the longest sentence we currently recognise"
-    speech(response)
-
 # convert dict to json and print to stdout
 print(json.dumps(o))
 
+def main():
+    fileName = save_voice_file()
+    intent = save_intent_to_file()
+    (profile, response) = VoiceAuthentication.FindBestMatch(fileName,)
+    isAllowed = PolicyHandler.comparePolicyWithProfile()
+    if (isAllowed[0]):
+        iisAllowed[1]
 
-
+if __name__ == '__main__':
+    main()
