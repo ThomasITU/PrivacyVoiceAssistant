9afc81819bf774a6bad88b1056528cf390d91d7e
ThomasITU <thwr@itu.dk>
2023-04-12 12:59:30 +0200

    update install script and audio devices

--- .devcontainer/scripts/InstallPython.sh
+++ .devcontainer/scripts/InstallPython.sh
@@ -6,19 +6,20 @@ apt install wget build-essential libncursesw5-dev libssl-dev -y\
 
 # download and install python 3.10.8
 
-directory=/privacyVoiceAssistant/lib/Python-3.10.8 
-if [ -d "$directory" ]; then
-    echo "$directory exists."
-else 
-    wget https://www.python.org/ftp/python/3.10.8/Python-3.10.8.tgz -O /privacyVoiceAssistant/lib//Python-3.10.8.tgz
-    cd /privacyVoiceAssistant/lib/
-    tar xzf Python-3.10.8.tgz 
-    rm Python-3.10.8.tgz
-    cd Python-3.10.8 
-    ./configure --enable-optimizations 
-    make altinstall 
-    cd /../privacyVoiceAssistant/
+directory=/privacyVoiceAssistant/lib/Python-3.10.8.tgz 
+if ! [ -d "$directory" ]; then
+    echo "$directory does not exists."
+    wget https://www.python.org/ftp/python/3.10.8/Python-3.10.8.tgz -O /privacyVoiceAssistant/lib//Python-3.10.8.tgz    
 fi
+
+echo "installing python 3.10.8"
+
+cd /privacyVoiceAssistant/lib/
+tar xzf Python-3.10.8.tgz 
+cd Python-3.10.8 
+./configure --enable-optimizations 
+make altinstall 
+cd /../privacyVoiceAssistant/
 
 
 python3.10 -m pip install speechbrain
