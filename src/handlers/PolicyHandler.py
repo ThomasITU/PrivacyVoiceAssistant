b4176dab4f4b4b29bb9f5f660b6da502f730f78c
ThomasITU <thwr@itu.dk>
2023-05-14 20:43:05 +0200

    fix the policyhandler and add test

--- src/handlers/PolicyHandler.py
+++ src/handlers/PolicyHandler.py
@@ -17,9 +17,9 @@ from model.Constant import Constant
 class PolicyHandler:
     
     def __init__(self, intent_dict):
-        if intent_dict is dict:
+        if isinstance(intent_dict, dict):
             self.intentDict = intent_dict
-        elif intent_dict is str:
+        elif isinstance(intent_dict,str):
             self.intentDict = parse_ini_file(intent_dict)
         else:
             raise ValueError("intent_dict must be either a dictionary or a string")
@@ -28,15 +28,21 @@ class PolicyHandler:
     def comparePolicyWithProfile(self, profile:Profile, intent:str) -> tuple[bool, Entity]:
         entities:dict = self.intentDict[intent]
         policies:list = profile.get_policy()
+
         print(policies)
         isSuccessfulEntity = (False, None) 
 
-        for p in policies:
-            entity = p.dataCommunicationRules.get_entity()
+        for policy in policies:
+            entity = policy.dataCommunicationRules.get_entity()
             if entity not in entities:
-                break
-            for purposes in p.dataCommunicationRules.dataUsageRules.get_purposes():
-                if purposes not in entities[entity]:
-                    break
+                continue
+
+            purposes = policy.dataCommunicationRules.dataUsageRules.get_purposes()
+            if not purposes:  
+                continue
+            
+            for purpose in purposes:
+                if purpose not in entities[entity]:
+                    continue
             isSuccessfulEntity = (True, entity)
         return isSuccessfulEntity
--- tests/test_PolicyHandler.py
+++ tests/test_PolicyHandler.py
@@ -40,6 +40,30 @@ def test_comparePolicyWithProfile_returns_proper_tuple():
 
     assert expectedTrue[0] == actualTrue[0]
     assert expectedTrue[1] == actualTrue[1]
-
     assert expectedFalse[0] == actualFalse[0]
     assert expectedFalse[1] == actualFalse[1]
+
+def test_comparePolicyWithProfile_given_profile_with_no_purposes_returns_false():
+
+    #Arrange
+    profile = _.dummyProfile()
+    dataUsageRules = DUR(set(),timestamp = datetime.datetime(1972, 1, 1))
+    dataCommunicationRule = DCR(set(),Entity.GOOGLE,dataUsageRules)
+    profile.get_policy()[0].dataCommunicationRules = dataCommunicationRule
+    expected = (False, None)
+    
+    #Arrange IntentDicts
+    dictPurposes = {Purpose.WEATHER, Purpose.CALENDER, Purpose.SEARCH}
+    dictEntities = {Entity.GOOGLE: dictPurposes,Entity.ALEXA: dictPurposes}
+    dictIntent = {"DummyIntent": dictEntities}
+
+    #Arrange PolicyHandler dictionaries
+    policyHandler = PolicyHandler(intent_dict=dictIntent)
+
+    #Act
+    actual = policyHandler.comparePolicyWithProfile(profile, "DummyIntent")
+
+    #Assert
+    assert expected[0] == actual[0]
+    assert expected[1] == actual[1]
+
